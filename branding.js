081ef607215b878fe93185549b61fbee1f754e3a
kezhili <kezhi.li@imperial.ac.uk>
2017-03-20 13:08:38 +0000

    upload js, jpg

---
+++ branding.js
@@ -0,0 +1,12 @@
+function ucbheader(){
+	document.write('<img src="ucb.png" border="0" usemap="#UCBMap" /><map name="UCBMap" id="UCBMap">
+	<area shape="rect" coords="11,3,298,27" href="http://www.imperial.ac.uk/" alt="Imperial College London" target="_blank" />
+	<area shape="rect" coords="794,7,828,25" href="http://www.imperial.ac.uk/" alt="IC Home" target="_blank" />
+	<area shape="rect" coords="835,7,873,25" href="http://www.imperial.ac.uk/search/" alt="IC Search" target="_blank"  />
+	<area shape="rect" coords="880,7,916,25" href="http://www.imperial.ac.uk/staff-development/about-us/a-z/" alt="IC A to Z" target="_blank"  />
+	<area shape="rect" coords="925,7,949,25" href="https://www.imperial.ac.uk/visit/campuses/" alt="Campus Map" target="_blank" /></map>');
+}
+
+function ucbfooter(){
+	document.write('<a target="_blank" href="http://www.imperial.ac.uk/"><img src="wordmark.png" alt="Imperial College London" /></a>');	
+}
